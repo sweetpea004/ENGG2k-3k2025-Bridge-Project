ad6defa75a5526d260ea66b89f731fee813f3f86
Emma Persephone Purvis <pklemma777@gmail.com>
2025-08-05 15:16:09 +1000

    moved programs over

---
+++ ENGG2k3kBridgePrograms/webapp/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, flash
+import multiprocessing
+import socket
+
+ESP_PORT = 5000
+APP_PORT = 5001
+BUF_SIZE = 4000
+VERBOSE = True # Controls whether messages are printed to console
+
+# Globals
+conn = False # boolean value representing if the app is connected to the ESP32
+
+app = Flask(__name__)
+
+class Status:
+    def __init__(self, array):
+
+        self.bridge_status = array[1].upper()
+        self.front_us = array[2].upper()
+        self.under_us = array[3].upper()
+        self.back_us = array[4].upper()
+        self.road_traf = array[5].upper()
+        self.road_lights = array[6].upper()
+        self.waterway_lights = array[7].upper()
+
+def receive() -> str:
+
+    # recieve string message 
+    data = b''
+    while True:
+        part = sock.recv(BUF_SIZE)
+        data += part
+        if len(part) < BUF_SIZE: # Check if reached end of message
+            break
+
+    message = data.decode().strip()
+    if VERBOSE:
+        print("Received:", message)
+    return message
+
+def send(message: str):
+
+    # send message in strong form
+    if VERBOSE:
+        print("Sent:", message)
+    sock.sendall(bytes(f"{message}\n", encoding="utf-8"))
+
+@app.get('/')
+def redirect_home():
+    return render_template("dashboard.html", status=status, conn=conn)
+
+if __name__ == "__main__":
+
+    # Connection with ESP32
+    sock = socket.socket()
+    #sock.connect(("localhost", ESP_PORT))
+
+    default_status = "STAT CLOS NONE NONE NONE TRAF GO STOP"
+    status = Status(default_status.split(" "))
+    
+    # Thread 1: Running App
+    app.run(debug=True, port=APP_PORT)
+
+    # Thread 2: Communications
+
+    
