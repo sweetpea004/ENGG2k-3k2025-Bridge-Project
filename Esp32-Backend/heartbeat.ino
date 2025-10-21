74b44d729139ca53acf3c06983de99931cf7e548
5crownik007 <nikolapavkovic4@gmail.com>
2025-10-21 14:33:58 +1100

    All switch cases for readmssg

--- Esp32-Backend/heartbeat.ino
+++ Esp32-Backend/heartbeat.ino
@@ -84,7 +84,7 @@ void readMssg(String mssg) {
   // POST: read the message and run the appropriate response command
   switch (mssg.substr(0,4)) {
     case "REDY":
-      processCommand("REDY")
+      processCommand("REDY");
 
     case "OKOK":
       processCommand("OKOK");
@@ -95,20 +95,96 @@ void readMssg(String mssg) {
         String extract = mssg.substr(i*5, 4);
         switch (extract) {
           case "OPEN": 
+            switch (i){ 
+              case 1:
 
-          case "SHIP":
+              case 2:
 
-          case "TRAF":
+            }
 
-          case "TRIG":
+            case "CLOS":
+              switch (i){
+                case 1:
 
-          case "GOGO":
+                case 2:
+              }
 
-          case "STOP":
+            case "SHIP":
+              switch (i){ 
+                case 3:
 
-          case "SLOW":
+                case 4:
 
-          case "NONE":
+                case 5:
+
+              }
+
+            case "TRAF":
+              switch (i){ 
+                case 6:
+
+                case 7:
+
+              }
+
+            case "TRIG":
+              switch (i){ 
+                case 8:
+
+                case 9:
+
+                case 10:
+
+                case 11:
+
+              }
+
+            case "GOGO":
+              switch (i){ 
+                case 12:
+
+                case 13:
+                
+              }
+
+            case "STOP":
+              switch (i){ 
+                case 12:
+
+                case 13:
+                
+              }
+
+            case "SLOW":
+              switch (i){ 
+                case 12:
+
+                case 13:
+                
+              }
+
+            case "NONE":
+              switch (i){ 
+                case 3:
+
+                case 4:
+
+                case 5:
+                
+                case 6:
+
+                case 7:
+
+                case 8:
+
+                case 9:
+
+                case 10:
+                
+                case 11:
+
+                case 14:
+              }
                     
         }
 
@@ -225,6 +301,7 @@ void handleClient() {
   if (clientConnected && client.available()) {
     String command = client.readStringUntil('\n');
     command.trim();
+    lastHeartbeat = 0;
     Serial.println(command);
     processCommand(command);
   }
